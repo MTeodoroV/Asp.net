c9ccabfed15fdfca95f45f863cb9b137fedbc357
Mateus T <mateusteodorovieirabaptista@gmail.com>
2022-05-05 23:54:00 -0300

    Subindo o projeto

---
+++ movie_store/app/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace app.Controllers;
+public class HomeController : Controller
+{
+    public ViewResult Index(int? id)
+    {
+        return View(id);
+    }
+}
